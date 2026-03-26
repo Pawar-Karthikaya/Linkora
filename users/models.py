65118e15bf6c6144d6bfb44109439ac75846c302
Pawar-Karthikaya <karthikayapawar114@gmail.com>
2026-03-26 14:41:44 +0530

    commit 26-03-2026

--- users/models.py
+++ users/models.py
@@ -15,7 +15,11 @@ class CountryCode(models.Model):
 class User(AbstractUser):
     # allready existing fields in AbstractUser:
     # username, first_name, last_name, email, password, is_staff, is_superuser, groups, permissions
-    country_code = models.ForeignKey(CountryCode, on_delete=models.PROTECT, null=False, blank=False)
+    country_code = models.ForeignKey(
+        CountryCode,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     bio = models.TextField(blank=True)
--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import User, CountryCode
 from .serializers import UserSerializer, CounterCodeSerializer
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                {
+                    "success": False,
+                    "error": serializer.errors
+                },
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer.save()
+        return Response(
+            {
+                "sucsess": True,
+                "Data": serializer.data
+            },
+            status.HTTP_201_CREATED    
+        )
